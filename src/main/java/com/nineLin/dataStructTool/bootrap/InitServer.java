4323657549da6ed11be664ecc198ac69acd56c45
java-vic <vic@Liuxy.local>
2015-03-06 17:29:05 +0800

    add some logs

--- src/main/java/com/nineLin/dataStructTool/bootrap/InitServer.java
+++ src/main/java/com/nineLin/dataStructTool/bootrap/InitServer.java
@@ -1,11 +1,17 @@
 package com.nineLin.dataStructTool.bootrap;
 
+import com.nineLin.dataStructTool.core.db.DBUtil;
+import com.nineLin.dataStructTool.core.db.SQLConnector;
+import com.nineLin.dataStructTool.core.file.SQLFileWriter;
 import com.nineLin.dataStructTool.core.plugin.IPlugin;
+import com.nineLin.dataStructTool.core.plugin.PropertiesPlugin;
+import com.nineLin.dataStructTool.core.reader.ExcelReader;
 import com.nineLin.dataStructTool.util.ClassSeacher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by vic on 15-3-4.
@@ -15,6 +21,10 @@ public class InitServer {
 
     public static void main(String[] args){
         initPlugins();
+        Map<String, List<String>> map = ExcelReader.readExcel(PropertiesPlugin.getStringValue("excelPath"));
+        SQLFileWriter.getInstance().writeSqlMap2Files(map);
+        DBUtil dbUtil = new DBUtil(PropertiesPlugin.getIntValueWithDefault("batchSize", 200));
+        dbUtil.writeSQLMap(SQLConnector.getConnection(), map);
     }
 
     public static void initPlugins(){
--- src/main/java/com/nineLin/dataStructTool/core/file/SQLFileWriter.java
+++ src/main/java/com/nineLin/dataStructTool/core/file/SQLFileWriter.java
@@ -1,9 +1,10 @@
 package com.nineLin.dataStructTool.core.file;
 
 import com.nineLin.dataStructTool.core.plugin.PropertiesPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -15,6 +16,9 @@ import java.util.Map;
  * Created by vic on 15-3-6.
  */
 public class SQLFileWriter {
+
+    private static final Logger log = LoggerFactory.getLogger("sqlFileWriter");
+
     // sqlFile path
     private String filePath;
 
@@ -31,25 +35,28 @@ public class SQLFileWriter {
 
     /**
      * write a map of sql into files
+     *
      * @param sqlMap
      */
-    public void writeSqlMap2Files(final Map<String, List<String>> sqlMap){
+    public void writeSqlMap2Files(final Map<String, List<String>> sqlMap) {
         File path = new File(filePath);
-        if(path == null || !path.isDirectory()){
+        if (path == null || !path.isDirectory()) {
             return;
         }
-        for(Map.Entry<String, List<String>> entry : sqlMap.entrySet()){
+        for (Map.Entry<String, List<String>> entry : sqlMap.entrySet()) {
             writeSqlList2File(entry.getKey(), entry.getValue());
         }
     }
 
     /**
      * write a list of sql into one file
+     *
      * @param tableName
      * @param sqlList
      */
     public void writeSqlList2File(final String tableName, final List<String> sqlList) {
-        File file = new File(filePath + tableName + ".sql");
+        final String fileName = new StringBuffer(tableName).append(".sql").toString();
+        File file = new File(filePath + fileName);
         FileOutputStream fos = null;
         if (file.exists()) {
             file.delete();
@@ -67,7 +74,9 @@ public class SQLFileWriter {
             }
             buffer.flip();
             fos.getChannel().write(buffer);
+            log.info(fileName + " file writing is finish...");
         } catch (IOException e) {
+            log.error(fileName + "writing error!!!");
             e.printStackTrace();
         } finally {
             try {
